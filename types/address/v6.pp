bd7f21bba2494e5b934f10edddb37b9bc03010c3
skrussell <russell@samknows.com>
2017-09-15 10:06:40 +0100

    Make compatible with puppetserver 5.1

--- types/address/v6.pp
+++ types/address/v6.pp
@@ -2,5 +2,5 @@ type IP::Address::V6 = Variant[
   IP::Address::V6::Full,
   IP::Address::V6::Compressed,
   IP::Address::V6::Alternative,
-  IP::Address::V6::NoSubnet,
+  IP::Address::V6::NoSubnet
 ]
